9c98e807e87cc2519d88d1b108e626aa51954f2e
nfomon <github_nfomon@doubleplum.net>
2013-06-10 19:29:44 -0400

    Reformatting/cleanups, and note where we'll need to escape single-quotes in token values.

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,27 +1,29 @@
-#include <fstream>
-#include <iostream>
-
 #include "tiny_lexer_st"
 
-int main(int argc, char* argv[]) {
-  using namespace std;
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
 
+namespace {
+  const string PROGRAM_NAME = "lush_lexer";
+}
+
+int main(int argc, char* argv[]) {
   if (argc != 1) {
-    cout << "usage: " << argv[0] << endl;
+    cout << "usage: " << PROGRAM_NAME << endl;
     return 1;
   }
 
   quex::Token token;
-  // Zero pointer to constructor --> use raw memory
-  quex::tiny_lexer_st  qlex((QUEX_TYPE_CHARACTER*)0x0, 0);
+  quex::tiny_lexer_st qlex((QUEX_TYPE_CHARACTER*)0x0, 0);
 
   int line_number = 0;
-  int number_of_tokens = 0;
-  (void)qlex.token_p_switch(&token);
-  while(cin) {
+  int num_tokens = 0;
+  qlex.token_p_switch(&token);
+  while (cin) {
     qlex.buffer_fill_region_prepare();
 
-    // Read a line from standard input
     cin.getline((char*)qlex.buffer_fill_region_begin(),
                 qlex.buffer_fill_region_size());
     if(cin.gcount() == 0) {
@@ -29,29 +31,28 @@ int main(int argc, char* argv[]) {
     }
     ++line_number;
 
-    // Inform about number of read characters. Note, that getline
-    // writes a terminating zero, which has not to be part of the
-    // buffer content.
-    qlex.buffer_fill_region_finish(cin.gcount() - 1);
+    qlex.buffer_fill_region_finish(cin.gcount()-1);
 
-    // Loop until the 'termination' token arrives
-    (void)qlex.receive();
+    qlex.receive();
     cout << line_number;
     while (token.type_id() != QUEX_TKN_TERMINATION && token.type_id() != QUEX_TKN_EXIT) {
       // serialize token
       cout << " " << token.column_number() << ":" << token.type_id_name();
       if (token.get_text().length() > 0) {
+        // escape '\' and '\'' in the value string with '\'
+        //string tvalue = token.get_text();
+        //size_t i = tvalue.find('\\'
         cout << ":'" << token.get_text().c_str() << "'";
       }
 
-      ++number_of_tokens;
-      (void)qlex.receive();
+      ++num_tokens;
+      qlex.receive();
     }
     cout << endl;
 
-    if(token.type_id() == QUEX_TKN_EXIT) break;
+    if (QUEX_TKN_EXIT == token.type_id()) break;
   }
 
-  cout << "Processed " << number_of_tokens << " tokens." << endl;
+  cout << "Processed " << num_tokens << " tokens." << endl;
   return 0;
 }
