8fd763c46f8899c7f5ea1cf7d8815707e776e30f
nfomon <github_nfomon@doubleplum.net>
2015-09-19 12:02:17 -0400

    Improved Star test case

--- statik/test/Star.cpp
+++ statik/test/Star.cpp
@@ -131,7 +131,12 @@ void Star::run() {
       test(root.GetState().GetStation(), statik::State::ST_DONE, "a");
       ip.ExtractChanges(out_batch);
       {
+        // Both items are deleted, then the remaining one is added back.
+        // That seems ok.
         Batch expected_batch;
+        List xca("a", "");
+        expected_batch.Insert(xca, NULL);
+        expected_batch.Delete(ca3);
         expected_batch.Delete(ca2);
         test(out_batch, expected_batch, "output");
       }
